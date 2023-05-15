9e8054e2ac78e885c9ec8e73b42d8c1469488c96
ahmedbilal-7191 <75791273+ahmedbilal-7191@users.noreply.github.com>
2023-05-15 20:14:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Create a variable “Skill Safari” and perform the following tasks.
+// 1) Find the length of the string.
+// 2) Convert the all the letters in the string into capital letters.
+// 3) What is the index position of “a”
+// Create a single consol.log that will display a famous conversation between Captain America
+// and Iron Man in Avengers. All the exchange should be in different lines.
+// Create a Body Mass Index logic by using Height and weight as inputs.
+
+var str = "Skill Safari"
+// 1) Find the length of the string.
+console.log("the length o string is " + str.length)
+// 2) Convert the all the letters in the string into capital letters.
+console.log(str.toUpperCase())
+// 3) What is the index position of “a”
+console.log("The index position of 'a'in the string is: " + str.indexOf("a"))
+// Create a single consol.log that will display a famous conversation between Captain America
+// and Iron Man in Avengers. All the exchange should be in different lines.
+console.log("Steve Rogers: \"Big man in a suit of armour. Take that off, what are you?\" \n Tony Stark: \"Genius, billionaire, playboy, philanthropist.\"")
+// Create a Body Mass Index logic by using Height and weight as inputs.
+
+// weight in kg
+// height in cm /100 for meterss
+// BMI = (weight) / (height * height)
+let weight_kg=60
+let height_mts=1.52
+let bmi=(weight_kg)/(height_mts*height_mts)
+console.log(bmi)
+
+
+
